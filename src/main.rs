6f31db17e6fbba8d8f289b04bb5520872e08612c
mrjackinc <mrjackinc@gmail.com>
2016-11-26 20:13:42 -0800

    End to end almost done

--- src/main.rs
+++ src/main.rs
@@ -14,124 +14,255 @@ extern crate env_logger;
 use std::sync::{Arc, RwLock};
 use std::{thread, time};
 
-struct SimulationMemory
-{
-    // Serialized steps
-    steps: Vec<String>,
-}
-
-// This names are terrible (TODO)
-struct ServiceMemory
-{
-    simulations: Vec<SimulationMemory>
-}
-
-
-
 mod polymini_server_state
 {
     use std::sync::{Arc, RwLock};
     use std::{thread, time};
-    use polyminis_core::control::*;
+    use std::collections::{HashMap, HashSet};
+
+    use polyminis_core::actuators::*;
     use polyminis_core::environment::*;
+    use polyminis_core::evaluation::*;
+    use polyminis_core::genetics::*;
     use polyminis_core::morphology::*;
     use polyminis_core::polymini::*;
+    use polyminis_core::sensors::*;
     use polyminis_core::serialization::*;
+    use polyminis_core::simulation::*;
     use polyminis_core::species::*;
+    use polyminis_core::traits::*;
 
     pub struct WorkerThreadState
     {
         kill: bool,
-        steps: Vec<String>,
-        static_state: String,
+        simulation_state: String,
+        epochs: Vec<EpochState>,
     }
     impl WorkerThreadState
     {
         fn new() -> WorkerThreadState
         {
-            WorkerThreadState { kill: false, steps: vec![], static_state: "".to_string() }
+            WorkerThreadState { kill: false, epochs: vec![], simulation_state: "".to_string() }
         }
 
-        fn worker_thread_main(workspace: Arc<RwLock<WorkerThreadState>>)
+        fn worker_thread_main(_: Json,
+                              workspace: Arc<RwLock<WorkerThreadState>>)
         {
-           let chromosomes = vec![[0, 0x09, 0x6A, 0xAD],
-                                  [0, 0x0B, 0xBE, 0xDA],
-                                  [0,    0, 0xBE, 0xEF],
-                                  [0,    0, 0xDB, 0xAD]];
 
-            let p1 = Polymini::new(Morphology::new(chromosomes, &TranslationTable::new()),
-                                   Control::new());
-            let mut sim = Simulation::new();
-            sim.add_species(Species::new(vec![p1]));
-            sim.add_object((10.0, 2.0), (1, 1));
+            let mut sim = Simulation::new(); 
+            let mut master_translation_table = HashMap::new();
 
-            // Critical Section
+            master_translation_table.insert( (TraitTier::TierI, 1), PolyminiTrait::PolyminiSimpleTrait(PolyminiSimpleTrait::SpeedTrait));
+            master_translation_table.insert( (TraitTier::TierI, 2), PolyminiTrait::PolyminiActuator(ActuatorTag::MoveHorizontal));
+            master_translation_table.insert( (TraitTier::TierI, 3), PolyminiTrait::PolyminiActuator(ActuatorTag::MoveVertical));
+
+            let mut active_table_1 = HashSet::new();
+            active_table_1.insert( (TraitTier::TierI, 3) );
+            active_table_1.insert( (TraitTier::TierI, 2) );
+            active_table_1.insert( (TraitTier::TierI, 1) );
+
+            let mut active_table_2 = HashSet::new();
+            active_table_2.insert( (TraitTier::TierI, 3) );
+            active_table_2.insert( (TraitTier::TierI, 2) );
+            active_table_2.insert( (TraitTier::TierI, 1) );
+
+
+            let default_sensors = vec![ Sensor::new(SensorTag::PositionX, 1),
+                                        Sensor::new(SensorTag::PositionY, 1),
+                                        Sensor::new(SensorTag::Orientation, 1),
+                                        Sensor::new(SensorTag::LastMoveSucceded, 1)];
+
+            let evaluators = vec![ FitnessEvaluator::OverallMovement,
+                                   FitnessEvaluator::DistanceTravelled,
+                                   FitnessEvaluator::Shape,
+                                   FitnessEvaluator::Alive, ];
+
+            let translation_table_species_1 = TranslationTable::new_from(&master_translation_table, &active_table_1);
+            let translation_table_species_2 = TranslationTable::new_from(&master_translation_table, &active_table_2);
+            let translation_table_species_3 = TranslationTable::new_from(&master_translation_table, &active_table_2);
+
+
+            let env = Environment::new(2, default_sensors);
+
+            let gens_per_epoch = 100;
+
+            let cfg = PGAConfig { max_generations: gens_per_epoch, population_size: 50,
+                                  percentage_elitism: 0.2, percentage_mutation: 0.1, fitness_evaluators: evaluators, genome_size: 8 };
+
+            trace!("Creating Species");
+            let s1 = Species::new_from("Test Species 1".to_owned(), translation_table_species_1,
+                                       &env.default_sensors, cfg.clone());
+
+
+            let s2 = Species::new_from("Test Species 2".to_owned(), translation_table_species_2,
+                                       &env.default_sensors, cfg.clone());
+
+
+            let s3 = Species::new_from("Test Species 3".to_owned(), translation_table_species_3,
+                                       &env.default_sensors, cfg.clone());
+
+            trace!("Adding Species");
+            let mut epoch = SimulationEpoch::new_with(env, gens_per_epoch as usize);
+            epoch.add_species(s1);
+            epoch.add_species(s2);
+            epoch.add_species(s3);
+            
+            trace!("Swaping Species:");
+            sim.swap_epoch(epoch);
+
+            let total_epochs = 20;
+
+            let mut kill = false;
+            'outer: for i in 0..total_epochs
             {
-                let mut w = workspace.write().unwrap();
-                w.static_state = sim.serialize(&mut SerializationCtx::new_from_flags(PolyminiSerializationFlags::PM_SF_STATIC)).to_string();
-            }
-
-            //TODO: Maybe some set of stopping criteria
-            while true
-            {
-                sim.step();
-                // Critical Section
                 {
                     let mut w = workspace.write().unwrap();
+                    w.epochs.push(EpochState::new(i));
+                }
+                loop
+                {
+                    if !sim.step()
+                    {
+                        let mut w = workspace.write().unwrap();
 
-                    if w.kill
+                        if w.kill
+                        {
+                            kill = true;
+                            break 'outer;
+                        }
+
+                        let step_json = sim.get_epoch().serialize(&mut SerializationCtx::new_from_flags(PolyminiSerializationFlags::PM_SF_DYNAMIC));
+                        w.epochs[i].steps.push(step_json);
+                    }
+                    else
                     {
                         break;
                     }
-
-                    let step_string = sim.serialize(&mut SerializationCtx::new_from_flags(PolyminiSerializationFlags::PM_SF_DYNAMIC)).to_string();
-                    w.steps.push(step_string);
                 }
 
-                let five_s = time::Duration::from_millis(5000);
-                thread::sleep(five_s);
+                sim.get_epoch_mut().evaluate_species(); 
+                {
+                    let mut w = workspace.write().unwrap();
+                    w.epochs[i].evaluated = true;
+
+                    for s in sim.get_epoch().get_species()
+                    {
+                        w.epochs[i].evaluation_data.insert(s.get_name().clone(), s.get_best().serialize(&mut SerializationCtx::new_from_flags(PolyminiSerializationFlags::PM_SF_STATS)));
+                    }
+                }
+
+                if i < total_epochs - 1
+                {
+                    sim.advance_epoch();
+                }
+            }
+
+            if kill
+            {
+                // Kill simulation
             }
         }
     }
 
+
+    #[derive(Clone)]
+    pub struct EpochState
+    {
+        epoch_num: usize,
+        steps: pmJsonArray,
+        evaluated: bool,
+        evaluation_data: pmJsonObject,
+    }
+    impl EpochState
+    {
+        pub fn new(i: usize) -> EpochState
+        {
+            EpochState { epoch_num: i, steps: pmJsonArray::new(), evaluated: false, evaluation_data: pmJsonObject::new() }
+        }
+        pub fn serialize(&self) -> Json
+        {
+            let mut json_obj = pmJsonObject::new();
+            json_obj.insert("Epoch".to_owned(),     self.epoch_num.to_json());
+            json_obj.insert("Steps".to_owned(),     self.steps.len().to_json());
+            json_obj.insert("Evaluated".to_owned(), self.evaluated.to_json());
+
+            if (self.evaluated)
+            {
+                json_obj.insert("EvaluationData".to_owned(), Json::Object(self.evaluation_data.clone()));
+            }
+            Json::Object(json_obj)
+        }
+    }
+    
     #[derive(Clone)]
     pub struct SimulationState
     {
-        static_state: String,
         work_thread_state: Arc<RwLock<WorkerThreadState>>,
     }
     impl SimulationState
     {
         pub fn new(worker_thread_state: &Arc<RwLock<WorkerThreadState>>) -> SimulationState
         {
-            let wt = worker_thread_state.clone(); 
-            let ss;
-            {
-                let w = wt.read().unwrap();
-                ss = w.static_state.clone();
-            }
-            SimulationState { static_state: ss, work_thread_state: wt }
+            SimulationState { work_thread_state: worker_thread_state.clone() }
         }
-        pub fn get_static_state(&self) -> String
+
+        pub fn serialize(&self) -> Json 
         {
-            let result: String;
+            let mut json_obj = pmJsonObject::new();
             {
                 let ws = self.work_thread_state.read().unwrap();
-                result = ws.static_state.clone();
+
             }
-            result.to_owned()
+            Json::Object(json_obj)
         }
-        pub fn get_dynamic_state(&self) -> Vec<String>
+
+        pub fn serialize_epoch_all(&self) -> Json
         {
-            let result: Vec<String>;
+            let mut json_arr = pmJsonArray::new();
 
             // Critical Section
             {
                 let ws = self.work_thread_state.read().unwrap();
-                result = ws.steps.clone();
+                for e in &ws.epochs
+                {
+                    json_arr.push(e.serialize());
+                }
             }
 
-            result
+            Json::Array(json_arr)
+        }
+
+        pub fn serialize_epoch(&self, e: usize) -> Json
+        {
+            let json;
+            {
+                let ws = self.work_thread_state.read().unwrap();
+                json = ws.epochs[e].serialize();
+            }
+            json
+        }
+
+        pub fn serialize_step_all(&self, e: usize) -> Json
+        {
+            let mut json_arr = pmJsonArray::new();
+            {
+                let ws = self.work_thread_state.read().unwrap();
+                for s in &ws.epochs[e].steps
+                {
+                    json_arr.push(s.clone());
+                }
+            }
+            Json::Array(json_arr)
+
+        }
+        pub fn serialize_step(&self, e: usize, s: usize) -> Json
+        {
+            let json;
+            {
+                let ws = self.work_thread_state.read().unwrap();
+                json = ws.epochs[e].steps[s].clone();
+            }
+            json
         }
     }
 
@@ -159,20 +290,17 @@ mod polymini_server_state
             let thread_copy = workspace.clone();
             thread::spawn(move ||
             {
-                WorkerThreadState::worker_thread_main(thread_copy);
+                WorkerThreadState::worker_thread_main(Json::Object(pmJsonObject::new()), thread_copy);
             });
         }
 
-        pub fn get_simulation_state_by_inx(&self, i: usize) -> (String, Vec<String>)
+        pub fn serialize(&self) -> Json
         {
-            if  i < self.simulations.len()
-            {
-                (self.simulations[i].get_static_state().clone(), self.simulations[i].get_dynamic_state())
-            }
-            else
-            {
-                ("".to_string(), vec![])
-            }
+            let mut json_obj = pmJsonObject::new();
+
+            json_obj.insert("NumSimulations".to_owned(), self.simulations.len().to_json());
+
+            Json::Object(json_obj)
         }
     }
 }
@@ -274,8 +402,27 @@ mod polymini_server_endpoints
             match *self
             {
                 //TODO:
-                Simulation::SimulationStateAll {ref s } => { return; },
-                Simulation::SimulationStateOne { ref s } => { return; },
+                Simulation::SimulationStateAll {ref s } =>
+                {
+                    // This should return information about all
+                    // the simulations in the server.
+                     response.send(s.serialize().to_string());
+                     return;
+                },
+                Simulation::SimulationStateOne { ref s } =>
+                { 
+                    // This should return information about this particular simulation;
+                    if let Some(sim_inx) = simulation_num 
+                    {
+                        let sim = &s.simulations[sim_inx];
+                        response.send(sim.serialize().to_string());
+                    }
+                    else
+                    {
+                        EndpointHelper::send_error(Error::NotFound, response);
+                    }
+                    return;
+                },
                 _ => {},
                 //~TODO:
             }
@@ -289,16 +436,22 @@ mod polymini_server_endpoints
 
             match *self
             {
-                Simulation::EpochStateAll {ref s } =>
+                Simulation::EpochStateAll { ref s } =>
                 {
+                    // Return all the Epochs for a particular simulation
+                    let sim = &s.simulations[simulation_index];
+                    response.send(sim.serialize_epoch_all().to_string());
                     return;
                 },
 
                 Simulation::EpochStateOne { ref s } =>
                 {
+                    // Return the state for a particular Epoch in a particular simulation
                     if let Some(e_num) = epoch_num
                     {
-
+                        let sim = &s.simulations[simulation_index];
+                        response.send(sim.serialize_epoch(e_num).to_string());
+                        return;
                     }
                     else
                     {
@@ -321,30 +474,15 @@ mod polymini_server_endpoints
                 Simulation::StepStateAll { ref s } =>
                 {
                     let sim = &s.simulations[simulation_index];
-                    let static_str = sim.get_static_state().clone();
-                    let mut simulation_dump: String = "".to_owned();
-                    simulation_dump = simulation_dump + &static_str;
-                    for ss in &sim.get_dynamic_state()
-                    {
-                        simulation_dump = simulation_dump + ss;
-                    }
-                    response.send(simulation_dump);
+                    response.send(sim.serialize_step_all(epoch).to_string());
+                    return;
                 },
                 Simulation::StepStateOne { ref s } =>
                 {
                     if let Some(step_n) = step_num
                     {
                         let sim = &s.simulations[simulation_index];
-                        let steps = sim.get_dynamic_state();
-                        if steps.len() < step_n
-                        {
-                            response.send("Simulation step not ready yet");
-                        }
-                        else
-                        {
-                            let step_string = sim.get_dynamic_state()[step_n - 1].clone();
-                            response.send(step_string);
-                        }
+                        response.send(sim.serialize_step(epoch, step_n).to_string());
                     }
                     else
                     {
